e72cda9ac94bd509937ba627f9325a1510b1b6ed
你的名字 <f1137171@tumt.edu.tw>
2026-03-31 23:34:28 +0800

    修正HTML內容

--- app.py
+++ app.py
@@ -6,17 +6,11 @@ app = Flask(__name__)
 app.register_blueprint(api_bp)
 
 @app.route("/")
-def home():
-    return "Bingo 系統運行中"
+def index():
+    return render_template("index.html")
 
 print("準備啟動 Flask")
 
 if __name__ == "__main__":
     print("進入 main")
     app.run(host="0.0.0.0", port=10000)
-
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
